b054df38de8d10b399c7f7d1c29294409f13712e
Christos <giachoud@gapps.auth.gr>
2023-04-30 17:33:03 +0300

    Max frequency of change

--- angle_finder.py
+++ angle_finder.py
@@ -1,32 +1,42 @@
 import cv2
 import numpy as np
-from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
 
 def findRotationAngle(img):
 
     angle = 0
 
     # Blur the input image
-    blurred_img = cv2.blur(img, (15, 15), cv2.BORDER_DEFAULT) 
+    blurred_img = cv2.blur(img, (18, 18), cv2.BORDER_DEFAULT) 
     cv2.imshow('blurred_img', blurred_img)
 
-    # Compute the discrete Fourier Transform of the image
-    fourier = cv2.dft(np.float32(blurred_img), flags=cv2.DFT_COMPLEX_OUTPUT)
-    
-    # Shift the zero-frequency component to the center of the spectrum
-    fourier_shift = np.fft.fftshift(fourier)
 
-    # calculate the magnitude of the Fourier Transform
-    magnitude = 20*np.log(cv2.magnitude(fourier_shift[:,:,0],fourier_shift[:,:,1]))
-    
-    # Scale the magnitude for display
-    magnitude = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+
+    # Compute the 2D Fourier Transform of the input image
+    fourier = np.fft.fft2(blurred_img)
+
+    # Shift the zero frequency component to the center of the spectrum
+    fourier_shifted = np.fft.fftshift(fourier)
+
+    # Compute the magnitude of the spectrum
+    magnitude_spectrum = 20 * np.log(np.abs(fourier_shifted))
+
+    # Find the maximum frequency of change in the magnitude spectrum
+    max_freq = np.argmax(magnitude_spectrum)
+
+    # Compute the row and column indices of the maximum frequency of change
+    max_row, max_col = np.unravel_index(max_freq, fourier_shifted.shape)
+    print(max_row, max_col)
 
     # Display the magnitude of the Fourier Transform
-    cv2.imshow('Fourier Transform', magnitude)
+    cv2.imshow('Magnitude Spectrum', magnitude_spectrum.astype(np.uint8))
 
-
+    plt.subplot(121), plt.imshow(blurred_img, cmap = 'gray')
+    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(magnitude_spectrum, cmap = 'gray')
+    plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+    plt.show()
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()

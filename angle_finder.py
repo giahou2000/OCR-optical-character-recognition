bc2d33be9bf5430e675caccce062dc3ceb22cd6c
Christos <giachoud@gapps.auth.gr>
2023-05-16 22:50:02 +0300

    yml

--- angle_finder.py
+++ angle_finder.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import math
 import imutils
 
 def findRotationAngle(img):
--- rotation.py
+++ rotation.py
@@ -1,4 +1,3 @@
-import cv2
 import imutils
 
 def rotateImage(x, angle):

679db2e2a271a26ec09c8f656a401e2b2225ec3c
Christos <giachoud@gapps.auth.gr>
2023-05-16 22:39:28 +0300

    Final upload

--- rotation.py
+++ rotation.py
@@ -3,7 +3,7 @@ import imutils
 
 def rotateImage(x, angle):
     y = imutils.rotate(x, angle=angle)
-    cv2.imshow('rotated image', y)
+    # cv2.imshow('rotated image', y)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
     return y
